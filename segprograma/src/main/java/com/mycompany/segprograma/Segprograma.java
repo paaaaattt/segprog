743567ef2d72c46e014c571117fca74c827b2521
paaaaattt <145470405+paaaaattt@users.noreply.github.com>
2023-09-26 08:45:06 -0600

    Add files via upload

---
+++ segprograma/src/main/java/com/mycompany/segprograma/Segprograma.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.segprograma;
+import.java.util.Scanner;
+
+
+/**
+ *
+ * @author User
+ */
+public class Segprograma {
+
+    public static void main(String[] args) {
+        Scanner leer=new Scanner. (System.In);
+    }
+}
